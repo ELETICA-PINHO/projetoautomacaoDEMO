a7c558cc2869c50a32bb88f72184676952be4e81
ELETRI-CAPINHO <eletricapinho@gmail.com>
2020-09-06 01:16:36 -0300

    inserção de comentarios na class.control

--- class.contro.php
+++ class.contro.php
@@ -14,6 +14,14 @@ class Controle{
         $this->pdo = $pdo;
     }
 
+/* 
+a função getStatus_sala()
+
+retorna status do valor que está no bd  na coluna  status_sala  os 02 possíveis valores  RELE01ON  ou RELE010F
+
+*/
+
+
     public function getStatus_sala(){
         $sql = $this->pdo->prepare("SELECT * FROM status");
         $sql->execute();
@@ -22,6 +30,13 @@ class Controle{
             return $sql['status_sala'];
         }
     }
+
+    /* 
+a função getStatus_quarto()
+
+retorna status do valor que está no bd  na coluna  status_quarto  os 02 possíveis valores  RELE02ON  ou RELE020F
+
+*/
 
     public function getStatus_quarto(){
         $sql = $this->pdo->prepare("SELECT * FROM status");
@@ -32,11 +47,33 @@ class Controle{
         }
     }
 
+/* 
+a função setStatus_sala($p)
+
+efetua um update na tabela status no campo status_sala  com dois possíveis valores RELE01ON  ou RELE010F
+
+através da variável que é passado ao char a função 
+
+*/
+
+
+
     public function setStatus_sala($p){
         $sql = $this->pdo->prepare("UPDATE status SET status_sala = :status_sala");
         $sql->bindValue(":status_sala", $p);
         $sql->execute();
     }
+
+
+
+    /* 
+a função setStatus_quarto($p)
+
+efetua um update na tabela status no campo status_quarto  com dois possíveis valores RELE02ON  ou RELE020F
+
+através da variável que é passado ao char a função 
+
+*/
 
     public function setStatus_quarto($p){
         $sql = $this->pdo->prepare("UPDATE status SET status_quarto = :status_quarto");
--- status.sala.php
+++ status.sala.php
@@ -3,10 +3,13 @@
 include "class.contro.php";
 include "config.php";
 
+
 /*
 essa pagina tem a funçãode retornar ou exibir status da variael que está no banco que guarda estado do rele
-é usado class.controle para obter falor faz select simples.
-*/ 
+é usado class.controle para obter valor faz select simples.
+
+ */
+
 
 $controle = new Controle($pdo);
 $exibe =$controle->getStatus_sala();
